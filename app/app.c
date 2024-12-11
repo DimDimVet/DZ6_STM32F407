9b6b5c53b8bed9d0a970fc35b202bbc5c38138f1
ДимДим <1000rub@list.ru>
2024-12-11 15:31:08 +0300

    st111224

---
+++ app/app.c
@@ -0,0 +1,16 @@
+#include "app.h"
+
+#define TASK 2//1,2
+
+#if TASK==1
+
+
+#elif TASK==2
+
+
+#endif
+
+int main()
+{
+	
+}
---
+++ inc/app.h
@@ -0,0 +1,6 @@
+#ifndef APP_H_
+#define APP_H_
+
+#include "stm32f4xx.h" // Device header
+
+#endif
